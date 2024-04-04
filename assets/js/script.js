20309b57d62d245836404ca0d3123518b3b89a7b
mackemo <mackenzielmoore14@gmail.com>
2024-04-04 12:26:25 -0400

    calling all functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,5 +116,7 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    generateTaskId();
+    createTaskCard();
+    renderTaskList();
 });
